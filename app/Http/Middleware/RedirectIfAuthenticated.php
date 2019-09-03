531ca7dd53fd6d7c32b5e2baebeece167fd688a6
Niels Pedersen <nip@emgroup.dk>
2019-09-03 10:00:10 +0200

    :wrench: Add frontpage

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -44,7 +44,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return response()->redirectTo(route('index'));
+            return response()->redirectTo(route('dashboard'));
         }
 
         return $next($request);
