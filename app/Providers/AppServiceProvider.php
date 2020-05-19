2946d43e46144b54d7b3edec53899296d4b3c688
thomasgenster <thomasgenster@gmail.com>
2020-05-19 15:27:18 +0200

    force https scheme if in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,7 +39,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        if ('heroku' === config('app.env')) {
+        if ('heroku' === config('app.env') || env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
