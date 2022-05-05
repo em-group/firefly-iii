2c003e63c329cadec399118613dc42e8316ab3bb
Niels Pedersen <nip@emgroup.dk>
2022-05-05 11:23:43 +0200

    feat(sites): added exp

--- webpack.mix.js
+++ webpack.mix.js
@@ -161,3 +161,6 @@ mix.copy('resources/assets/frontpage-templates/BT/img/', 'public/fpt/BT/img');
 // CB
 mix.sass('resources/assets/frontpage-templates/CB/sass/styles.scss', 'public/fpt/CB/css');
 mix.copy('resources/assets/frontpage-templates/CB/img/', 'public/fpt/CB/img');
+// EXP
+mix.sass('resources/assets/frontpage-templates/EXP/sass/styles.scss', 'public/fpt/EXP/css');
+mix.copy('resources/assets/frontpage-templates/EXP/img/', 'public/fpt/EXP/img');
